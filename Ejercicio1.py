9e407b282146e9c5ce38a16b49f54ad1470d7991
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-10-18 18:25:39 +0000

    totalmente terminado

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,7 +1,10 @@
 #Ejercicio 1:Utilicice las funciones anteriores para agregar elementos faltantes
 def ejercicio1():
-    lista = ["p", "t"]
-    lista.append("h")
-    lista.insert("d")
-    lista.extend(["i", "j", "k"])
-    assert "".join(lista) == "Python"
+  lista = ["P", "t"]
+  lista.append("h")
+  lista.insert(1,"y")
+  lista.extend(["o","n"])
+  print("lista =", lista)
+  assert "".join(lista) == "Python"
+  return
+  
