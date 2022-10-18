323baa5e1925db83960377cf9d25a9d5a7c5459b
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-10-18 19:19:03 +0000

    totalmente terminado ahora

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,9 +1,8 @@
 #Ejercicio 2: Utilice las funciones anteriores a para eliminar elementos sobrantes
 def ejercicio2():
-  lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  del lista[1]
-  lista.remove(6)
-  lista.pop()
-  assert lista == list(range(1,6))
-  return
-  
+    lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    del lista[7]
+    lista.remove(8)
+    lista.pop()
+    assert lista == list(range(1, 6))
+    return
